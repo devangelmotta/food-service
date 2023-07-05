940dbdbaf9115a19b7f3b0a6e2f588e88cf3d95f
Angel Motta <devangelmotta@gmail.com>
2023-07-05 10:13:22 -0500

    testing adding region

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,7 @@ import { RecommendationsModule } from './recommendations/recommendations.module'
       aws: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID,
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-        region: process.env.AWS_REGION
+        region: process.env.AWS_REGION || "us-east-1"
       },
       logger: true
     }),
